181c38d18bc438e734ffb8d391e5418c36a2f335
vins0nc <vins0n@proton.me>
2025-06-01 13:23:10 -0700

    Creates OpenGL WIndow

---
+++ src/Main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Render", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    /*
+    ********************************************************************************************************
+    =========================================== Render Loop Start ==========================================
+    ********************************************************************************************************
+    */
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // Clear the screen with a background color
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);
+    }
+    /*
+    ********************************************************************************************************
+    ============================================ Render Loop End ============================================
+    *********************************************************************************************************
+    */
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
